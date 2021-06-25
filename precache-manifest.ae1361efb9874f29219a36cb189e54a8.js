9cb8fdc60b159a1c69b3adeaf69c350292feda11
kat <kataleksandrova@gmail.com>
2021-06-24 21:28:11 -0700

    deploy

---
+++ precache-manifest.ae1361efb9874f29219a36cb189e54a8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b5d7e7c891558f25b00",
+    "url": "/wedding_website_ghpages/css/app.dcb50a64.css"
+  },
+  {
+    "revision": "7b5d7e7c891558f25b00",
+    "url": "/wedding_website_ghpages/js/app.d05709ca.js"
+  },
+  {
+    "revision": "85f7378069e802f80957",
+    "url": "/wedding_website_ghpages/js/chunk-vendors.1b485898.js"
+  },
+  {
+    "revision": "9b541d1c8812a3af1d55431c2929856b",
+    "url": "/wedding_website_ghpages/img/flowers_left.9b541d1c.png"
+  },
+  {
+    "revision": "42e297b1da70a9d338181d2520bc1809",
+    "url": "/wedding_website_ghpages/img/flowers_right.42e297b1.png"
+  },
+  {
+    "revision": "afd1eb17097bc0f2176534cd6f9c99fd",
+    "url": "/wedding_website_ghpages/img/loading_cat.afd1eb17.gif"
+  },
+  {
+    "revision": "3e718128994284b69bf75a0f1ba3735a",
+    "url": "/wedding_website_ghpages/img/kat-danny-engagement-110.3e718128.jpg"
+  },
+  {
+    "revision": "ba7602ddde4962704427df97186af644",
+    "url": "/wedding_website_ghpages/index.html"
+  },
+  {
+    "revision": "9b541d1c8812a3af1d55431c2929856b",
+    "url": "/wedding_website_ghpages/img/flowers_left.png"
+  },
+  {
+    "revision": "42e297b1da70a9d338181d2520bc1809",
+    "url": "/wedding_website_ghpages/img/flowers_right.png"
+  },
+  {
+    "revision": "afd1eb17097bc0f2176534cd6f9c99fd",
+    "url": "/wedding_website_ghpages/img/loading_cat.gif"
+  },
+  {
+    "revision": "879f9f5725826f57c00d8f514fc470c1",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-1.png"
+  },
+  {
+    "revision": "95643f1b209851253900a0532287b598",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-2.png"
+  },
+  {
+    "revision": "009d7be347ecb82df8194cca3154c1a0",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-6.png"
+  },
+  {
+    "revision": "4b3b0671bb2e648d7e66ff3616f02bd2",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-4.png"
+  },
+  {
+    "revision": "f4640ff25d7708219993a646ab67fde4",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-5.png"
+  },
+  {
+    "revision": "b2328b4e9fbe250f9d7b7df8add3a88f",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-3.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wedding_website_ghpages/robots.txt"
+  },
+  {
+    "revision": "674910889c43045c6302687a0abe7f85",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-bg.jpg"
+  },
+  {
+    "revision": "af885dde2f2166d8a89701df205e6d9b",
+    "url": "/wedding_website_ghpages/img/peachy-flowers-1.tif"
+  }
+];
